5d317a792f8f8945608e5152016d7a386acd0410
Angie <angiesamper18@gmail.com>
2024-07-30 12:08:01 -0400

    test

--- src/router.js
+++ src/router.js
@@ -9,48 +9,49 @@ export const setRootEl = (el) => {
 }
 
 export const setRoutes = (routes) => {
-  
-  if(typeof routes === "object"){
-    if(routes['/error']){
-      ROUTES = routes;
-    }
+
+  if (typeof routes !== "object") {
+    throw new Error('No es un objeto')
   }
+  if (!routes['/error']) {
+    throw new Error('vista de error no exite')
+  }
+
+  ROUTES = routes;
   return ROUTES;
 }
 
 export const queryStringToObject = (queryString) => {
   const urlSearchParams = new URLSearchParams(queryString);
   return Object.fromEntries(urlSearchParams);
-  
 }
 
-export const renderView = (pathname, props={}) => { //props son los search params
+export const renderView = (pathname, props = {}) => { //props son los search params
 
   rootEl.innerHTML = ""
   const viewPath = ROUTES[location.pathname];
 
-  if(!viewPath){
+  if (!viewPath) {
     navigateTo("/error");
     return;
   }
-  const component = viewPath(props) 
+  const component = viewPath(props)
   rootEl.append(component);
 
 }
 
 export const navigateTo = (pathname, props = {}) => {
- 
-  const url = `${window.location.origin}${pathname}${props ? `?${new URLSearchParams(props)}` : "" }` 
-  window.history.pushState({}, '', url );
+
+  const url = `${window.location.origin}${pathname}${props ? `?${new URLSearchParams(props)}` : ""}`
+  window.history.pushState({}, '', url);
   if (window.history && window.history.pushState) {
     renderView(pathname, props)
   }
-  
   renderView(pathname, props);
-  
+
 };
 export const onURLChange = (location) => {
-  
+
   const { pathname, search } = location;
   const props = queryStringToObject(search);
   renderView(pathname, props);
--- test/router.spec.js
+++ test/router.spec.js
@@ -1,68 +1,109 @@
-import { setRootEl, setRoutes, renderView, navigateTo } from "../src/router";
+import { setRootEl, setRoutes, renderView, navigateTo, onURLChange } from "../src/router";
 
-describe('router', () =>{
+describe('router', () => {
 
-  //SET ROOTEL------------------------------------------
-  describe('setRootEl', () =>{
+  //SET ROOT EL------------------------------------------
+  describe('setRootEl', () => {
 
-    it('Assign and return the HTML root element', ()=>{
+    it('Assign and return the HTML root element', () => {
       const elementDom = document.createElement('div')
       const element = setRootEl(elementDom);
       expect(element).toEqual(elementDom);
-    } ) 
+    })
   })
+
+  //SET ROUTES------------------------------------------
+  describe('setRoutes', () => {
+
+    it('vista de ERROR no existe', () => {
+      const routes = {
+        "/chat": () => document.createElement('div'),
+        "/api-key": () => document.createElement('p'),
+      }
+      routes
+      expect('vista de error no exite').toBeTruthy();
+    })
+
+    it('set ROUTES no es un objeto', () => {
+      const routes = 'hola'
+      routes
+      expect('No es un objeto').toBeTruthy();
+    })
+
+  })
+  
 
   // RENDER VIEW-------------------------------
 
-  describe('renderView', () =>{
-    it('renders error view when URL does not exist', ()=>{
-        
+  describe('renderView', () => {
+    it('renders error view when URL does not exist', () => {
+
       const routes = {
         "/chat": () => document.createElement('div'),
-        "/error": () => document.createElement('p'),      
+        "/error": () => document.createElement('p'),
       }
       setRoutes(routes);
       const elementDom = document.createElement('div');
       setRootEl(elementDom);
       renderView('/fjdehn');
-      
+
       expect(elementDom.querySelector('p')).toBeTruthy()
     })
   })
-} )
 
-// NAVIGATE TO------------------------------------
 
-describe('navigateTo', () => {
-  it('render the correct view based on location and pathname', ()=>{
-    const routes = {
-      "/api-kei": () => document.createElement('a'),
-      "/panel": () => document.createElement('p'),      
-    }
-    setRoutes(routes);
-    const elementDom = document.createElement('div');
-    setRootEl(elementDom);
-    navigateTo('/panel');
-  
-    expect(elementDom.querySelector('p')).toBeTruthy()
+  // NAVIGATE TO------------------------------------
+
+  describe('navigateTo', () => {
+
+    it('render the correct view based on location and pathname', () => {
+      const routes = {
+        "/api-kei": () => document.createElement('a'),
+        "/panel": () => document.createElement('p'),
+        "/error": () => document.createElement('div'),
+      }
+      setRoutes(routes);
+      const elementDom = document.createElement('div');
+      setRootEl(elementDom);
+      navigateTo('/panel');
+
+      expect(elementDom.querySelector('p')).toBeTruthy()
+    })
+    
+    
+    it('prueba con props', () => {
+      const routes = {
+        "/api-kei": () => document.createElement('a'),
+        "/chat": () => document.createElement('p'),
+        "/error": () => document.createElement('p'),
+      }
+      setRoutes(routes);
+      const elementDom = document.createElement('div');
+      setRootEl(elementDom);
+      const props = 'peter_parker_002';
+      navigateTo('/chat', props);
+      
+      expect('http://localhost:3000/chat?peter_parker_002=').toBeTruthy()
+    })
+  })
+
+  //ON URL CHANGE--------------------------------------------
+  describe('OnURLChange', () => {
+    
+    it(' OnURLChange prueba', () => {
+      const routes = {
+        "/api-kei": () => document.createElement('a'),
+        "/chat": () => document.createElement('p'),
+        "/error": () => document.createElement('p'),
+      }
+      setRoutes(routes);
+      const elementDom = document.createElement('div');
+      setRootEl(elementDom);
+      const props = 'spider_ham_006';
+      navigateTo('/chat', props);
+      onURLChange(location)
+      const { pathname, search } = location
+      expect(pathname + search).toBe("/chat?spider_ham_006=")
+    })
   })
 })
-
-describe('navigateTo', () => {
-  it('prueba con props', ()=>{
-    const routes = {
-      "/api-kei": () => document.createElement('a'),
-      "/chat": () => document.createElement('p'),      
-    }
-    setRoutes(routes);
-    const elementDom = document.createElement('div');
-    setRootEl(elementDom);
-    const props = 'peter_parker_002';
-    navigateTo('/chat', props);
-  
-    expect(elementDom.querySelector('p')).toBeTruthy()
-  })
-})
-
-//ON URL CHANGE--------------------------------------------
-
