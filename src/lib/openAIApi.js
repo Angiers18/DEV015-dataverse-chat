52334e17f3125479c62879a9c11c1dde9432e867
Angie <angiesamper18@gmail.com>
2024-08-01 19:22:48 -0400

    style: delete code comments

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -26,39 +26,3 @@ export const communicateWithOpenAI = (messages) => {
       console.error('ERROOOR DE OPEN AI' + error)
     })
 }
-
-
-
-
-// Importa la función para obtener la API KEY desde apiKey.js
-// import { getApiKey } from './apiKey.js';
-
-// const apiKey = getApiKey;
-// const apiOpenAI = 'https://api.openai.com/v1/chat/compleciones';
-
-// export const communicateWithOpenAI = (messages) => {
-//   //Aquí es donde debes implementar la petición con fetch o axios
-
-//   const promise = fetch(apiOpenAI, {
-
-//     method: 'POST',
-//     header: {
-//       'Authorization': `Bearer ${apiKey}`,
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       model: 'gpt-4',
-//       messages: messages,
-//     })
-//       .then(response => {
-//         return response.json()
-//       })
-//       .then(data => console.log(data))
-//       .catch((error) => {
-//         console.error('ERROOOR DE OPEN AI' + error)
-//       }),
-
-//   })
-//   return promise
-
-// }
