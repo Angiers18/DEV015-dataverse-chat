6f24f9c8684b3911f2856bf6446ea663c0dfa11e
Angie <angiesamper18@gmail.com>
2024-08-07 17:03:22 -0400

    feat: function communicateWithOpenAI follow the conversation history and unit tests apiKey

--- src/lib/openAIApi.js
+++ src/lib/openAIApi.js
@@ -1,37 +1,41 @@
 //Importa la función para obtener la API KEY desde apiKey.js
 import { getApiKey } from "./apiKey.js";
 
-const SentMessage = [];
+let SentMessage = [];
 
 export const communicateWithOpenAI = (character, message) => {
   const apiKey = getApiKey();
   const apiOpenAI = "https://api.openai.com/v1/chat/completions";
 
-  const header = {
+  if (!apiKey) {
+    return Promise.reject(new Error("No se encontró una API key"));
+  }
+
+  // Agrega el mensaje del usuario al historial
+  SentMessage.push({ role: "user", content: message });
+
+  const headers = {
     "Content-Type": "application/json",
-    Authorization: `Bearer ${apiKey}`,
+    Authorization: `Bearer ${apiKey}`
   };
+
   const data = {
     model: "gpt-4",
     messages: [
-      { role: "system", content: `Eres ${character.name}, tu personalidad se basa en ${character.description}; vas a responder a los mensajes y preguntas del usuario y tus respuestas no van a tener más de 20 palabras`, SentMessage },
-      { role: "user", content: message }
+      {
+        role: "system", content: `Eres ${character.name}, tu personalidad se basa en ${character.description}; vas a responder a los mensajes y preguntas del usuario y tus respuestas no van a tener más de 20 palabras`
+      }, ...SentMessage // Incluye todo el historial de mensajes
     ]
   };
-  SentMessage.push({ role: "user", content: message })
-
-  if (!apiKey) {
-    return Promise.reject(new Error("No se encontro una apikey"));
-  }
 
   return fetch(apiOpenAI, {
     method: "POST",
-    headers: header,
-    body: JSON.stringify(data),
+    headers: headers,
+    body: JSON.stringify(data)
   })
-    .then((response) => {
+    .then(response => {
       if (!response.ok) {
-        return response.json().then((error) => {
+        return response.json().then(error => {
           throw new Error(`Error: ${response.status} - ${error.error.message}`);
         });
       }
@@ -39,11 +43,13 @@ export const communicateWithOpenAI = (character, message) => {
     })
     .then(data => {
       const reply = data.choices[0].message.content;
-      // console.log("User's prompt:", data.messages[1].content);
       console.log("Assistant's reply:", reply);
-      SentMessage.push({role: 'system', reply})
-      console.log( 'conversacion:', SentMessage)
-      return reply
+
+      // Agrega la respuesta del asistente al historial
+      SentMessage.push({ role: "assistant", content: reply });
+
+      console.log('Conversación:', SentMessage);
+      return reply;
     })
-    .catch(error => console.error("ERROOOR DE OPEN AI" + error))
-};
+    .catch(error => console.error("Error de OpenAI:", error));
+}
